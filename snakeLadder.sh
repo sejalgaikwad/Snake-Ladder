3f8c8580a80ab4fdf4d8bc693295278c916f0bb7
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-12 15:54:06 +0530

    Added: create a Random to check for option

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -6,4 +6,21 @@ position=0
 
 diceRandom=$(($RANDOM%6 +1 ))
 
+option=$(($RANDOM%3))
+
+case $option in
+
+	0)
+		position=0
+	;;
+	1)
+		position=$(($position + $diceRandom))
+	;;
+	2)
+		position=$(($position - $diceRandom))
+	;;
+esac
+
 echo $diceRandom
+echo $option
+echo $position
